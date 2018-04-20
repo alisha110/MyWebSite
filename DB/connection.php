3fb7028bed45b593929f74b939a25796c5da4c1f
alisha110 <ali.sha.11080@gmail.com>
2018-04-20 17:56:05 +0430

    سرو سامان دادن به سایت ولود کردن صفحه اول

---
+++ DB/connection.php
@@ -0,0 +1,15 @@
+<?php
+try
+{
+    $server_name = "localhost";
+    $server_user = "root";
+    $server_password = "";
+    $name_db = "winoot";
+    global $link;
+    $link  = new PDO("mysql:host=$server_name;dbname=$name_db;charset=utf8", "$server_user", "$server_password");
+
+}
+catch (PDOException $e)
+{
+    echo $e->getMessage();
+}
